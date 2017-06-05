7c42b0eda428ebed5309854e145e9bf753809dfc
nixswinner <nixswinner@gmail.com>
2017-06-05 10:22:33 +0300

    Creating a new activity to show the selected food calories and enabling the home button for easy navigations

--- app/src/main/java/com/tergech/nixon/foodncalories/MainActivity.java
+++ app/src/main/java/com/tergech/nixon/foodncalories/MainActivity.java
@@ -1,9 +1,11 @@
 package com.tergech.nixon.foodncalories;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        //actionBar.setHomeButtonEnabled(true);
-        //actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
         pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), Titles, Numboftabs);
 
@@ -30,5 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent myIntent = new Intent(getApplicationContext(), ShowTotalCalo.class);
+                startActivityForResult(myIntent, 0);
+                MainActivity.this.finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        
     }
 }
--- app/src/main/java/com/tergech/nixon/foodncalories/ShowTotalCalo.java
+++ app/src/main/java/com/tergech/nixon/foodncalories/ShowTotalCalo.java
@@ -1,8 +1,34 @@
 package com.tergech.nixon.foodncalories;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 /**
  * Created by Tonui on 6/5/2017.
  */
 
-public class ShowTotalCalo {
+public class ShowTotalCalo extends AppCompatActivity {
+    private TextView tvcalo;
+    private ImageButton btnAdd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.showcalo);
+        tvcalo=(TextView)findViewById(R.id.tvcalo);
+        btnAdd=(ImageButton)findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvcalo.setText("Hello its working");
+                Intent intent= new Intent(ShowTotalCalo.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
